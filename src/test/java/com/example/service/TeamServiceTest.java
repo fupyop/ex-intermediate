6eeb484339e957d6e9497720ad6c5ba0fc467a94
fupyop <fuka.hirayama@rakus-partners.co.jp>
2020-02-12 09:30:49 +0900

    クラスを作成しました。

---
+++ src/test/java/com/example/service/TeamServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TeamServiceTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testshowList() {
+		assertThat("チーム一覧が正しく表示されてません",showList ,comparesEqualTo());
+	}
+	
+	@Test
+	public void testshowDetail() {
+		assertThat("チーム詳細が正しく表示されてません",showDetail, comparesEqualTo());
+	}
+
+}
